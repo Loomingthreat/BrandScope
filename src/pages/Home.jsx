3c7eb3e79b1a48acf5a4223b3d98e3151366d059
Henrique O. <foxonsnow@gmail.com>
2025-07-25 15:11:57 -0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-[80vh]">
+      <h2 className="text-4xl font-bold">Welcome to BrandScope</h2>
+    </div>
+  );
+}
